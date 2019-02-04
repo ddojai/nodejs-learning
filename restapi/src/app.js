82b06c5a978593ca5fb8151cd0271efbcf4c9099
김흥열 <favianred@gmail.com>
2019-02-04 23:22:41 +0900

    * 아하 REST API 서버 개발 (4)

--- restapi/src/app.js
+++ restapi/src/app.js
@@ -4,7 +4,7 @@ import createError from 'http-errors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import indexRouter from './routes/index';
+import v1Route from './routes/v1';
 
 const app = express();
 
@@ -12,21 +12,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', indexRouter);
+
+app.use('/v1', v1Route);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
+  let apiError = err;
+
+  if (!err.status) {
+    apiError = createError(err);
+  }
+
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
+  res.locals.message = apiError.message;
+  res.locals.error = process.env.NODE_ENV === 'development' ? apiError : {};
 
   // render the error page
-  return res.status(err.status || 500).json(res.locals.error)
+  return res.status(apiError.status)
+    .json({ message: apiError.message })
 });
 
 // bin/www 를 그대로 사용하기 위해서 예외적으로 commonJs 문법을 적용
