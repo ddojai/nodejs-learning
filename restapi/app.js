fee5d628c281f2b48f6ea6926cecce2617e86735
김흥열 <favianred@gmail.com>
2019-02-04 22:08:29 +0900

    * 아하 REST API 서버 개발 (3)

--- restapi/app.js
+++ restapi/app.js
@@ -1,21 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import createError from 'http-errors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,8 +26,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  return res.status(err.status || 500).json(res.locals.error)
 });
 
+// bin/www 를 그대로 사용하기 위해서 예외적으로 commonJs 문법을 적용
 module.exports = app;
