092c7ee3c055349776ae912cf6c0737ad556e95a
김흥열 <favianred@gmail.com>
2019-02-05 23:27:55 +0900

    * 아하 REST API 서버 개발 (5)

--- restapi/src/controllers/v1/user.controller.js
+++ restapi/src/controllers/v1/user.controller.js
@@ -1,7 +1,10 @@
-const get = (req, res, next) => {
+import { models } from '../../models'
+
+const get = async (req, res, next) => {
   try {
-    console.log(req.query.rest)
-  return res.json({ message: 'users get' });
+    const users = await models.User.findAll()
+
+    return res.json(users)
   } catch (e) {
     next(e)
   }
