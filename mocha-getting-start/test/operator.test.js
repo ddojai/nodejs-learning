583ce66ca49349437cf557d258c634cfdb4941e2
ddojai <favianred@gmail.com>
2019-03-03 14:54:48 +0900

    * mocha-getting-start   * [TDD] 2. Chai - Getting started

--- mocha-getting-start/test/operator.test.js
+++ mocha-getting-start/test/operator.test.js
@@ -1,5 +1,7 @@
 const operator = require('../src/operator');
 const sum = operator.sum;
+const chai = require('chai');
+const expect = chai.expect;
 
 describe('operator.js Test', () => {
     describe('sum 함수 테스트', () => {
@@ -11,11 +13,8 @@ describe('operator.js Test', () => {
         });
         it ('10 + 20 = 30', done => {
             const result = sum(10, 20);
-            if (result === 30) {
-                done();
-            } else {
-                throw Error('결과 값이 30이 아니다');
-            }
+            expect(result).to.equal(30);
+            done();
         });
         after(() => {
             console.log('after')
