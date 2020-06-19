278d7864dcdc986e9470b90c2219e330bde81d64
ddojai <favianred@gmail.com>
2020-06-19 23:22:22 +0900

    Solve the two-sum-ii-input-array-is-sorted of leetcode

---
+++ javascript/leetcode/two-sum-ii-input-array-is-sorted/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1];
+      }
+    }
+  }
+};
+
+const numbers = [2,3,4];
+const target = 6;
+
+const result = twoSum(numbers, target);
+console.log(result);
